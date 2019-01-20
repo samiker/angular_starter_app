19b93a05af89434b35bf753c74b86a6e53b0fee2
Sami Kerboute <sami@kerboute.com>
2019-01-20 09:27:35 +0100

    feat: CRUD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'smartapp';
+  onUpdate = false;
+  title = 'Sami Kerboute';
+  // courses = ['laravel', 'symfony', 'donet', 'windev', 'jee'];
+  courses = [
+    { id: 1, title: "Laravel", active: true },
+    { id: 2, title: "Angular", active: true },
+    { id: 3, title: "Symfony", active: false },
+    { id: 4, title: "Vuejs", active: true }
+  ];
+  currentCourse = {
+    id: 0,
+    title: "",
+    active: false
+  };
+
+  addCourse() {
+    this.courses.unshift(this.currentCourse);
+    this.currentCourse = {
+      id: 0,
+      title: "",
+      active: false
+    };
+  }
+
+  editCourse(course){
+    this.onUpdate = true;
+    this.currentCourse = course;
+  }
+
+  removeCourse(course) {
+    let r = confirm("Vous etes sur ?");
+    if (r == true) {
+      let index = this.courses.indexOf(course);
+      this.courses.splice(index, 1);
+    }
+  }
+
+  updateCourse(course){
+    this.onUpdate = false;
+  }
+
+  togglePower(course){
+    course.active = !course.active;
+  }
+
 }
